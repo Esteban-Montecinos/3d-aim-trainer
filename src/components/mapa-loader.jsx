15216b4421009a06311443ea24ac7dcaad42e1a8
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2024-01-02 12:34:05 -0300

    fix: loader

--- src/components/mapa-loader.jsx
+++ src/components/mapa-loader.jsx
@@ -1,9 +1,7 @@
 export default function MapaLoader() {
   return (
-    <div className='absolute left-0 flex flex-col items-center justify-center size-full bg-neutral-900'>
-        <div>
-            <h2 className="text-xl">cargando mapa...</h2>
-        </div>
+    <div className="absolute top-0 left-0 z-30 flex flex-col items-center justify-center size-full bg-neutral-900">
+      <h2 className="text-xl">cargando mapa...</h2>
     </div>
-  )
+  );
 }
